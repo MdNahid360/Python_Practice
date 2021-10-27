6158636788755db94f211a7672d07acf36e20183
MdNahid360 <mdnahid360s@gmail.com>
2021-10-28 00:33:46 +0600

    prfac python

---
+++ main.py
@@ -0,0 +1,6 @@
+import math
+def areaFunction(radius):
+         my_area = math.pi*radius**2
+         return my_area
+rediuS = int(input("Input Your radius  : "))
+print("Your redius is : ", rediuS)
---
+++ nahid1.py
@@ -0,0 +1,8 @@
+def msf():
+    n = 1
+    sum=0
+    for n in range (10):
+        if n%2==0
+            continue
+        sum=sum+n
+    print(sum)
